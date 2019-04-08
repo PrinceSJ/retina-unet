7b00350a914c4296ee53136253d992c9d28ccd3a
Moriaty <sj86033503@gmail.com>
2019-04-08 15:50:37 +0200

    20190408 working on resnet

--- src/retinaNN_training.py
+++ src/retinaNN_training.py
@@ -18,6 +18,7 @@ from keras.callbacks import ModelCheckpoint, LearningRateScheduler
 from keras import backend as K
 from keras.utils.vis_utils import plot_model as plot
 from keras.optimizers import SGD
+from keras.metrics import top_k_categorical_accuracy
 
 import sys
 sys.path.insert(0, './lib/')
@@ -101,6 +102,8 @@ def bottleneck_Block(inpt,nb_filters,strides=(1,1),with_conv_shortcut=False):
         x = add([x, inpt])
         return x
 
+def acc_top2(y_true, y_pred):
+    return top_k_categorical_accuracy(y_true, y_pred, k=2)
 
 def resnet_50(channel,height,width,classes=20):
     inpt = Input(shape=(width, height, channel))
@@ -127,16 +130,19 @@ def resnet_50(channel,height,width,classes=20):
     x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
     x = bottleneck_Block(x, nb_filters=[256, 256, 1024])
 
-    #conv5_x
+    #conv5_x  
     x = bottleneck_Block(x, nb_filters=[512, 512, 2048], strides=(2, 2), with_conv_shortcut=True)
     x = bottleneck_Block(x, nb_filters=[512, 512, 2048])
     x = bottleneck_Block(x, nb_filters=[512, 512, 2048])
 
-    x = AveragePooling2D(pool_size=(7, 7))(x)
+    x = AveragePooling2D(pool_size=(2, 2))(x)
     x = Flatten()(x)
     x = Dense(classes, activation='softmax')(x)
 
     model = Model(inputs=inpt, outputs=x)
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc',top_k_categorical_accuracy])
+
     return model
 
 #==================================================
@@ -144,64 +150,64 @@ def resnet_50(channel,height,width,classes=20):
 
 #Define the neural network gnet
 #you need change function call "get_unet" to "get_gnet" in line 166 before use this network
-def get_gnet(n_ch,patch_height,patch_width):
-    inputs = Input((n_ch, patch_height, patch_width))
-    conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
-    conv1 = Dropout(0.2)(conv1)
-    conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
-    up1 = UpSampling2D(size=(2, 2))(conv1)
-    #
-    conv2 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(up1)
-    conv2 = Dropout(0.2)(conv2)
-    conv2 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(conv2)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    #
-    conv3 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(pool1)
-    conv3 = Dropout(0.2)(conv3)
-    conv3 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv3)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    #
-    conv4 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool2)
-    conv4 = Dropout(0.2)(conv4)
-    conv4 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv4)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv4)
-    #
-    conv5 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool3)
-    conv5 = Dropout(0.2)(conv5)
-    conv5 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv5)
-    #
-    up2 = merge([UpSampling2D(size=(2, 2))(conv5), conv4], mode='concat', concat_axis=1)
-    conv6 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up2)
-    conv6 = Dropout(0.2)(conv6)
-    conv6 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv6)
-    #
-    up3 = merge([UpSampling2D(size=(2, 2))(conv6), conv3], mode='concat', concat_axis=1)
-    conv7 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up3)
-    conv7 = Dropout(0.2)(conv7)
-    conv7 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv7)
-    #
-    up4 = merge([UpSampling2D(size=(2, 2))(conv7), conv2], mode='concat', concat_axis=1)
-    conv8 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(up4)
-    conv8 = Dropout(0.2)(conv8)
-    conv8 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(conv8)
-    #
-    pool4 = MaxPooling2D(pool_size=(2, 2))(conv8)
-    conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(pool4)
-    conv9 = Dropout(0.2)(conv9)
-    conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv9)
-    #
-    conv10 = Convolution2D(2, 1, 1, activation='relu', border_mode='same')(conv9)
-    conv10 = core.Reshape((2,patch_height*patch_width))(conv10)
-    conv10 = core.Permute((2,1))(conv10)
-    ############
-    conv10 = core.Activation('softmax')(conv10)
+# def get_gnet(n_ch,patch_height,patch_width):
+#     inputs = Input((n_ch, patch_height, patch_width))
+#     conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(inputs)
+#     conv1 = Dropout(0.2)(conv1)
+#     conv1 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv1)
+#     up1 = UpSampling2D(size=(2, 2))(conv1)
+#     #
+#     conv2 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(up1)
+#     conv2 = Dropout(0.2)(conv2)
+#     conv2 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(conv2)
+#     pool1 = MaxPooling2D(pool_size=(2, 2))(conv2)
+#     #
+#     conv3 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(pool1)
+#     conv3 = Dropout(0.2)(conv3)
+#     conv3 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv3)
+#     pool2 = MaxPooling2D(pool_size=(2, 2))(conv3)
+#     #
+#     conv4 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(pool2)
+#     conv4 = Dropout(0.2)(conv4)
+#     conv4 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv4)
+#     pool3 = MaxPooling2D(pool_size=(2, 2))(conv4)
+#     #
+#     conv5 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(pool3)
+#     conv5 = Dropout(0.2)(conv5)
+#     conv5 = Convolution2D(128, 3, 3, activation='relu', border_mode='same')(conv5)
+#     #
+#     up2 = merge([UpSampling2D(size=(2, 2))(conv5), conv4], mode='concat', concat_axis=1)
+#     conv6 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(up2)
+#     conv6 = Dropout(0.2)(conv6)
+#     conv6 = Convolution2D(64, 3, 3, activation='relu', border_mode='same')(conv6)
+#     #
+#     up3 = merge([UpSampling2D(size=(2, 2))(conv6), conv3], mode='concat', concat_axis=1)
+#     conv7 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(up3)
+#     conv7 = Dropout(0.2)(conv7)
+#     conv7 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv7)
+#     #
+#     up4 = merge([UpSampling2D(size=(2, 2))(conv7), conv2], mode='concat', concat_axis=1)
+#     conv8 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(up4)
+#     conv8 = Dropout(0.2)(conv8)
+#     conv8 = Convolution2D(16, 3, 3, activation='relu', border_mode='same')(conv8)
+#     #
+#     pool4 = MaxPooling2D(pool_size=(2, 2))(conv8)
+#     conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(pool4)
+#     conv9 = Dropout(0.2)(conv9)
+#     conv9 = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(conv9)
+#     #
+#     conv10 = Convolution2D(2, 1, 1, activation='relu', border_mode='same')(conv9)
+#     conv10 = core.Reshape((2,patch_height*patch_width))(conv10)
+#     conv10 = core.Permute((2,1))(conv10)
+#     ############
+#     conv10 = core.Activation('softmax')(conv10)
 
-    model = Model(input=inputs, output=conv10)
+#     model = Model(input=inputs, output=conv10)
 
-    # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.3, nesterov=False)
-    model.compile(optimizer='sgd', loss='categorical_crossentropy',metrics=['accuracy'])
+#     # sgd = SGD(lr=0.01, decay=1e-6, momentum=0.3, nesterov=False)
+#     model.compile(optimizer='sgd', loss='categorical_crossentropy',metrics=['accuracy'])
 
-    return model
+#     return model
 
 #========= Load settings from Config file
 config = configparser.RawConfigParser()
@@ -237,8 +243,14 @@ visualize(group_images(patches_masks_train[0:N_sample,:,:,:],5),'./'+name_experi
 n_ch = patches_imgs_train.shape[1]
 patch_height = patches_imgs_train.shape[2]
 patch_width = patches_imgs_train.shape[3]
+
+
+#=========== Switch network =================
 #model = get_unet(n_ch, patch_height, patch_width)  #the U-net model
 model = resnet_50(n_ch, patch_height, patch_width)  #the ResNet model
+
+
+#=========== Construct and save the model arcitecture =====
 print("Check: final output of the network:")
 print(model.output_shape)
 plot(model, to_file='./'+name_experiment+'/'+name_experiment + '_model.png')   #check how the model looks like
@@ -261,7 +273,14 @@ checkpointer = ModelCheckpoint(filepath='./'+name_experiment+'/'+name_experiment
 # lrate_drop = LearningRateScheduler(step_decay)
 
 patches_masks_train = masks_Unet(patches_masks_train)  #reduce memory consumption
-model.fit(patches_imgs_train, patches_masks_train, nb_epoch=N_epochs, batch_size=batch_size, verbose=2, shuffle=True, validation_split=0.1, callbacks=[checkpointer])
+print("\nPatches_masks_train shape:"+str(patches_masks_train.shape))
+#============  Fit for different network ==================================
+#for unet
+#model.fit(patches_imgs_train, patches_masks_train, nb_epoch=N_epochs, batch_size=batch_size, verbose=2, shuffle=True, validation_split=0.1, callbacks=[checkpointer])  #unet
+
+#for resnet
+patches_imgs_train = np.transpose(patches_imgs_train,(0,3,2,1))
+model.fit(patches_imgs_train, patches_masks_train, nb_epoch=N_epochs, batch_size=batch_size, verbose=2, shuffle=True, validation_split=0.1, callbacks=[checkpointer])  #resnet50
 
 
 #========== Save and test the last model ===================
