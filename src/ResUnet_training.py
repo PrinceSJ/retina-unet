b6b3c245a5003f9614c514bb96c439a381b5b2d3
Moriaty <sj86033503@gmail.com>
2019-05-02 15:56:46 +0200

    Update: ResUnet

---
+++ src/ResUnet_training.py
@@ -0,0 +1,464 @@
+import numpy as np
+import pandas as pd
+import six
+import configparser
+
+from random import randint
+
+from sklearn.model_selection import train_test_split
+from skimage.transform import resize
+
+from keras import Model
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from keras.models import load_model
+from keras.optimizers import Adam, SGD
+from keras.utils.vis_utils import plot_model as plot
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Input, Conv2DTranspose, MaxPooling2D, concatenate, Dropout,BatchNormalization
+from keras.layers import Conv2D, Concatenate, core, Reshape, AveragePooling2D, Flatten
+from keras.layers import UpSampling2D, Dense, BatchNormalization, ZeroPadding2D
+from tqdm import tqdm_notebook
+from keras import initializers
+from keras import regularizers
+from keras import constraints
+from keras.utils import conv_utils
+from keras.utils.data_utils import get_file
+from keras.engine.topology import get_source_inputs
+from keras.engine import InputSpec
+from keras import backend as K
+from keras_applications.imagenet_utils import _obtain_input_shape
+from keras.regularizers import l2
+from keras.metrics import top_k_categorical_accuracy
+from keras.engine.topology import Input
+from keras.engine.training import Model
+from keras.layers.convolutional import Conv2D, UpSampling2D, Conv2DTranspose
+from keras.layers.core import Activation, SpatialDropout2D
+from keras.layers.merge import concatenate,add
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import MaxPooling2D
+from keras.losses import binary_crossentropy
+from keras import backend as K
+
+import sys
+sys.path.insert(0, './lib/')
+from help_functions import *
+
+#function to obtain data for training/testing (validation)
+from extract_patches import get_data_training
+
+
+#==================Define Network==============================================
+#============Build U-Net Model=================
+def handle_block_names(stage):
+    conv_name = 'decoder_stage{}_conv'.format(stage)
+    bn_name = 'decoder_stage{}_bn'.format(stage)
+    relu_name = 'decoder_stage{}_relu'.format(stage)
+    up_name = 'decoder_stage{}_upsample'.format(stage)
+    return conv_name, bn_name, relu_name, up_name
+
+
+def Upsample2D_block(filters, stage, kernel_size=(3,3), upsample_rate=(2,2),
+                     batchnorm=False, skip=None):
+
+    def layer(input_tensor):
+
+        conv_name, bn_name, relu_name, up_name = handle_block_names(stage)
+
+        x = UpSampling2D(size=upsample_rate, name=up_name)(input_tensor)
+
+        if skip is not None:
+            #print("x: "+str(x.shape)+"\nskip: "+str(skip.shape))
+            x = Concatenate()([x, skip])
+
+        x = Conv2D(filters, kernel_size, padding='same', name=conv_name+'1')(x)
+        if batchnorm:
+            x = BatchNormalization(name=bn_name+'1')(x)
+        x = Activation('relu', name=relu_name+'1')(x)
+
+        x = Conv2D(filters, kernel_size, padding='same', name=conv_name+'2')(x)
+        if batchnorm:
+            x = BatchNormalization(name=bn_name+'2')(x)
+        x = Activation('relu', name=relu_name+'2')(x)
+
+        return x
+    return layer
+
+
+def Transpose2D_block(filters, stage, kernel_size=(3,3), upsample_rate=(2,2),
+                      transpose_kernel_size=(4,4), batchnorm=False, skip=None):
+
+    def layer(input_tensor):
+
+        conv_name, bn_name, relu_name, up_name = handle_block_names(stage)
+
+        x = Conv2DTranspose(filters, transpose_kernel_size, strides=upsample_rate,
+                            padding='same', name=up_name)(input_tensor)
+        if batchnorm:
+            x = BatchNormalization(name=bn_name+'1')(x)
+        x = Activation('relu', name=relu_name+'1')(x)
+
+        if skip is not None:
+            x = Concatenate()([x, skip])
+
+        x = Conv2D(filters, kernel_size, padding='same', name=conv_name+'2')(x)
+        if batchnorm:
+            x = BatchNormalization(name=bn_name+'2')(x)
+        x = Activation('relu', name=relu_name+'2')(x)
+
+        return x
+    return layer
+
+def build_unet(backbone, classes, last_block_filters, skip_layers,
+               n_upsample_blocks=5, upsample_rates=(2,2,2,2,2),
+               block_type='upsampling', activation='sigmoid',
+               **kwargs):
+
+    input = backbone.input
+    x = backbone.output
+
+    if block_type == 'transpose':
+        up_block = Transpose2D_block
+    else:
+        up_block = Upsample2D_block
+    # convert layer names to indices
+    skip_layers = ([get_layer_number(backbone, l) if isinstance(l, str) else l
+                    for l in skip_layers])
+    
+    for i in range(n_upsample_blocks):
+        
+        # check if there is a skip connection
+        if i < len(skip_layers):
+#             print(backbone.layers[skip_layers[i]])
+            #print(backbone.layers[skip_layers[i]].output)
+            skip = backbone.layers[skip_layers[i]].output
+        else:
+            skip = None
+
+        up_size = (upsample_rates[i], upsample_rates[i])
+        filters = last_block_filters * 2**(n_upsample_blocks-(i+1))
+        #print("Hahah!")
+        x = up_block(filters, i, upsample_rate=up_size, skip=skip, **kwargs)(x)
+        #print("Wocao!!")
+    if classes < 2:
+        activation = 'sigmoid'
+   
+    x = Conv2D(classes, (3,3), padding='same', name='final_conv')(x)
+    x = Activation(activation, name=activation)(x)
+
+    model = Model(input, x)
+
+    return model
+
+#=============ResNet 34============
+def _bn_relu(input):
+    """Helper to build a BN -> relu block
+    """
+    norm = BatchNormalization(axis=CHANNEL_AXIS)(input)
+    return Activation("relu")(norm)
+
+
+def _conv_bn_relu(**conv_params):
+    """Helper to build a conv -> BN -> relu block
+    """
+    filters = conv_params["filters"]
+    kernel_size = conv_params["kernel_size"]
+    strides = conv_params.setdefault("strides", (1, 1))
+    kernel_initializer = conv_params.setdefault("kernel_initializer", "he_normal")
+    padding = conv_params.setdefault("padding", "same")
+    kernel_regularizer = conv_params.setdefault("kernel_regularizer", l2(1.e-4))
+
+    def f(input):
+        conv = Conv2D(filters=filters, kernel_size=kernel_size,
+                      strides=strides, padding=padding,
+                      kernel_initializer=kernel_initializer,
+                      kernel_regularizer=kernel_regularizer)(input)
+        return _bn_relu(conv)
+
+    return f
+
+
+def _bn_relu_conv(**conv_params):
+    """Helper to build a BN -> relu -> conv block.
+    This is an improved scheme proposed in http://arxiv.org/pdf/1603.05027v2.pdf
+    """
+    filters = conv_params["filters"]
+    kernel_size = conv_params["kernel_size"]
+    strides = conv_params.setdefault("strides", (1, 1))
+    kernel_initializer = conv_params.setdefault("kernel_initializer", "he_normal")
+    padding = conv_params.setdefault("padding", "same")
+    kernel_regularizer = conv_params.setdefault("kernel_regularizer", l2(1.e-4))
+
+    def f(input):
+        activation = _bn_relu(input)
+        return Conv2D(filters=filters, kernel_size=kernel_size,
+                      strides=strides, padding=padding,
+                      kernel_initializer=kernel_initializer,
+                      kernel_regularizer=kernel_regularizer)(activation)
+
+    return f
+
+
+def _shortcut(input, residual):
+    """Adds a shortcut between input and residual block and merges them with "sum"
+    """
+    # Expand channels of shortcut to match residual.
+    # Stride appropriately to match residual (width, height)
+    # Should be int if network architecture is correctly configured.
+    input_shape = K.int_shape(input)
+    residual_shape = K.int_shape(residual)
+    stride_width = int(round(input_shape[ROW_AXIS] / residual_shape[ROW_AXIS]))
+    stride_height = int(round(input_shape[COL_AXIS] / residual_shape[COL_AXIS]))
+    equal_channels = input_shape[CHANNEL_AXIS] == residual_shape[CHANNEL_AXIS]
+
+    shortcut = input
+    # 1 X 1 conv if shape is different. Else identity.
+    if stride_width > 1 or stride_height > 1 or not equal_channels:
+        shortcut = Conv2D(filters=residual_shape[CHANNEL_AXIS],
+                          kernel_size=(1, 1),
+                          strides=(stride_width, stride_height),
+                          padding="valid",
+                          kernel_initializer="he_normal",
+                          kernel_regularizer=l2(0.0001))(input)
+
+    return add([shortcut, residual])
+
+def basic_block(filters, init_strides=(1, 1), is_first_block_of_first_layer=False):
+    """Basic 3 X 3 convolution blocks for use on resnets with layers <= 34.
+    """
+    def f(input):
+
+        if is_first_block_of_first_layer:
+            # don't repeat bn->relu since we just did bn->relu->maxpool
+            conv1 = Conv2D(filters=filters, kernel_size=(3, 3),
+                           strides=init_strides,
+                           padding="same",
+                           kernel_initializer="he_normal",
+                           kernel_regularizer=l2(1e-4))(input)
+        else:
+            conv1 = _bn_relu_conv(filters=filters, kernel_size=(3, 3),
+                                  strides=init_strides)(input)
+
+        residual = _bn_relu_conv(filters=filters, kernel_size=(3, 3))(conv1)
+        return _shortcut(input, residual)
+
+    return f
+
+def _residual_block(block_function, filters, repetitions, is_first_layer=False):
+    """Builds a residual block with repeating bottleneck blocks.
+    """
+    def f(input):
+        for i in range(repetitions):
+            init_strides = (1, 1)
+            if i == 0 and not is_first_layer:
+                init_strides = (2, 2)
+            input = block_function(filters=filters, init_strides=init_strides,
+                                   is_first_block_of_first_layer=(is_first_layer and i == 0))(input)
+        return input
+
+    return f
+
+def _handle_dim_ordering():
+    global ROW_AXIS
+    global COL_AXIS
+    global CHANNEL_AXIS
+    if K.image_dim_ordering() == 'tf':
+        ROW_AXIS = 1
+        COL_AXIS = 2
+        CHANNEL_AXIS = 3
+    else:
+        CHANNEL_AXIS = 1
+        ROW_AXIS = 2
+        COL_AXIS = 3
+
+
+def _get_block(identifier):
+    if isinstance(identifier, six.string_types):
+        res = globals().get(identifier)
+        if not res:
+            raise ValueError('Invalid {}'.format(identifier))
+        return res
+    return identifier
+
+
+class ResnetBuilder(object):
+    @staticmethod
+    def build(input_shape, block_fn, repetitions,input_tensor):
+        _handle_dim_ordering()
+        if len(input_shape) != 3:
+            raise Exception("Input shape should be a tuple (nb_channels, nb_rows, nb_cols)")
+
+        # Permute dimension order if necessary
+        if K.image_dim_ordering() == 'tf':
+            input_shape = (input_shape[1], input_shape[2], input_shape[0])
+
+        # Load function from str if needed.
+        block_fn = _get_block(block_fn)
+        
+        if input_tensor is None:
+            img_input = Input(shape=input_shape)
+        else:
+            if not K.is_keras_tensor(input_tensor):
+                img_input = Input(tensor=input_tensor, shape=input_shape)
+            else:
+                img_input = input_tensor
+                
+        conv1 = _conv_bn_relu(filters=64, kernel_size=(7, 7), strides=(2, 2))(img_input)
+        pool1 = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding="same")(conv1)
+
+        block = pool1
+        filters = 64
+        for i, r in enumerate(repetitions):
+            block = _residual_block(block_fn, filters=filters, repetitions=r, is_first_layer=(i == 0))(block)
+            filters *= 2
+
+        # Last activation
+        block = _bn_relu(block)
+
+        model = Model(inputs=img_input, outputs=block)
+        return model
+
+    @staticmethod
+    def build_resnet_34(input_shape,input_tensor):
+        return ResnetBuilder.build(input_shape, basic_block, [3, 4, 6, 3],input_tensor)
+
+
+#=============U-Net with ResNet34 Encoder============
+def UResNet34(input_shape=(None, None, 3), classes=1, decoder_filters=16, decoder_block_type='upsampling',
+                       encoder_weights=None, input_tensor=None, activation='sigmoid', **kwargs):
+
+    backbone = ResnetBuilder.build_resnet_34(input_shape=input_shape,input_tensor=input_tensor)
+    
+    skip_connections = list([97,54,25])  # for resnet 34
+    #print("sc done")
+    model = build_unet(backbone, classes, decoder_filters,
+                       skip_connections, block_type=decoder_block_type,
+                       activation=activation, **kwargs)
+    #print("bu done")
+    model.name = 'u-resnet34'
+
+    return model
+
+#==============Define Loss Function===============
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred = K.cast(y_pred, 'float32')
+    y_pred_f = K.cast(K.greater(K.flatten(y_pred), 0.5), 'float32')
+    intersection = y_true_f * y_pred_f
+    score = 2. * K.sum(intersection) / (K.sum(y_true_f) + K.sum(y_pred_f))
+    return score
+
+def dice_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return 1. - score
+
+def bce_dice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+
+def bce_logdice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) - K.log(1. - dice_loss(y_true, y_pred))
+
+def weighted_bce_loss(y_true, y_pred, weight):
+    epsilon = 1e-7
+    y_pred = K.clip(y_pred, epsilon, 1. - epsilon)
+    logit_y_pred = K.log(y_pred / (1. - y_pred))
+    loss = weight * (logit_y_pred * (1. - y_true) + 
+                     K.log(1. + K.exp(-K.abs(logit_y_pred))) + K.maximum(-logit_y_pred, 0.))
+    return K.sum(loss) / K.sum(weight)
+
+def weighted_dice_loss(y_true, y_pred, weight):
+    smooth = 1.
+    w, m1, m2 = weight, y_true, y_pred
+    intersection = (m1 * m2)
+    score = (2. * K.sum(w * intersection) + smooth) / (K.sum(w * m1) + K.sum(w * m2) + smooth)
+    loss = 1. - K.sum(score)
+    return loss
+
+def weighted_bce_dice_loss(y_true, y_pred):
+    y_true = K.cast(y_true, 'float32')
+    y_pred = K.cast(y_pred, 'float32')
+    # if we want to get same size of output, kernel size must be odd
+    averaged_mask = K.pool2d(
+            y_true, pool_size=(50, 50), strides=(1, 1), padding='same', pool_mode='avg')
+    weight = K.ones_like(averaged_mask)
+    w0 = K.sum(weight)
+    weight = 5. * K.exp(-5. * K.abs(averaged_mask - 0.5))
+    w1 = K.sum(weight)
+    weight *= (w0 / w1)
+    loss = weighted_bce_loss(y_true, y_pred, weight) + dice_loss(y_true, y_pred)
+    return loss
+
+
+#========= Load settings from Config file
+config = configparser.RawConfigParser()
+config.read('configuration.txt')
+#patch to the datasets
+path_data = config.get('data paths', 'path_local')
+#Experiment name
+name_experiment = config.get('experiment name', 'name')
+#training settings
+epochs = int(config.get('training settings', 'N_epochs'))
+batch_size = int(config.get('training settings', 'batch_size'))
+
+#====================================================================================
+
+
+#============ Load the data and divided in patches
+patches_imgs_train, patches_masks_train = get_data_training(
+    DRIVE_train_imgs_original = path_data + config.get('data paths', 'train_imgs_original'),
+    DRIVE_train_groudTruth = path_data + config.get('data paths', 'train_groundTruth'),  #masks
+    patch_height = int(config.get('data attributes', 'patch_height')),
+    patch_width = int(config.get('data attributes', 'patch_width')),
+    N_subimgs = int(config.get('training settings', 'N_subimgs')),
+    inside_FOV = config.getboolean('training settings', 'inside_FOV') #select the patches only inside the FOV  (default == True)
+)
+
+#print("patches_imgs_train: "+str(patches_masks_train.shape))
+
+#========= Save a sample of what you're feeding to the neural network ==========
+N_sample = min(patches_imgs_train.shape[0],40)
+visualize(group_images(patches_imgs_train[0:N_sample,:,:,:],5),'./'+name_experiment+'/'+"sample_input_imgs")#.show()
+visualize(group_images(patches_masks_train[0:N_sample,:,:,:],5),'./'+name_experiment+'/'+"sample_input_masks")#.show()
+
+
+img_size_target = 128
+model = UResNet34(input_shape=(1,img_size_target,img_size_target))
+# model.summary()
+model.compile(loss=bce_dice_loss, optimizer="adam", metrics=["accuracy"])
+
+#=========== Construct and save the model arcitecture =====
+print("Check: final output of the network:")
+print(model.output_shape)
+plot(model, to_file='./'+name_experiment+'/'+name_experiment + '_model.png')   #check how the model looks like
+json_string = model.to_json()
+open('./'+name_experiment+'/'+name_experiment +'_architecture.json', 'w').write(json_string)
+
+#===============Training==============
+early_stopping = EarlyStopping(patience=10, verbose=1)
+#model_checkpoint = ModelCheckpoint("./keras.model", save_best_only=True, verbose=1)
+model_checkpoint = ModelCheckpoint(filepath='./'+name_experiment+'/'+name_experiment +'_best_weights.h5', verbose=1, monitor='loss', mode='auto', save_best_only=True) #save at each epoch if the validation decreased
+reduce_lr = ReduceLROnPlateau(factor=0.1, patience=4, min_lr=0.00001, verbose=1)
+
+patches_imgs_train = patches_imgs_train.transpose((0,3,2,1))
+patches_masks_train = patches_masks_train.transpose((0,3,2,1))
+history = model.fit(patches_imgs_train, patches_masks_train,
+                    #validation_data=[x_valid, y_valid], 
+                    epochs=epochs,
+                    batch_size=batch_size,
+                    callbacks=[early_stopping, model_checkpoint, reduce_lr],shuffle=True)
+
+# fig, (ax_loss, ax_acc) = plt.subplots(1, 2, figsize=(15,5))
+# ax_loss.plot(history.epoch, history.history["loss"], label="Train loss")
+# ax_loss.plot(history.epoch, history.history["val_loss"], label="Validation loss")
+# ax_acc.plot(history.epoch, history.history["acc"], label="Train accuracy")
+# ax_acc.plot(history.epoch, history.history["val_acc"], label="Validation accuracy")
+
+#=================Load best model================
+#model.load_weights('./keras.model')
+
+#========== Save and test the last model ===================
+model.save_weights('./'+name_experiment+'/'+name_experiment +'_last_weights.h5', overwrite=True)
+
